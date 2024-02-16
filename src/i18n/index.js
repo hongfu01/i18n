ba8917650c06b5709b8fe1c1872219b021a8566b
esan <apple@esanMac-mini.local>
2024-02-16 15:43:59 +0800

    add

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,6 +1,7 @@
-import { createI18n } from 'vue-i18n'
+import { createI18n,useI18n } from 'vue-i18n'
 import enus from './langs/en-US/index.js'
 import zhtw from './langs/zh-TW/index.js'
+
 
 const i18n = createI18n({
   locale: 'en',
@@ -9,5 +10,7 @@ const i18n = createI18n({
     zhtw: zhtw,
   }
 })
+
+export const $i18n = useI18n({ useScope: 'global' });
 
 export default i18n
