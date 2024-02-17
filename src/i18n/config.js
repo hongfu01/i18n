96cef795339e077a2117bab9e3c9a8763de29d90
esan <apple@esanMac-mini.local>
2024-02-17 11:31:45 +0800

    同步設定

---
+++ src/i18n/config.js
@@ -0,0 +1,106 @@
+/** @format */
+
+import enus from "./langs/en-US";
+
+export const tempCodes = {
+  enus: "en-US",
+  zhTw: "zh-TW",
+};
+
+export const countryNameMap = {
+  "en-US": "English",
+  "zh-TW": "中文繁體",
+};
+
+export let langsMenu = Object.keys(countryNameMap).map((key) => ({
+  name: countryNameMap[key],
+  langCode: key,
+  img: require(`../assets/image/countrys/${key}.png`)
+}));
+
+export const version = "v1.240201002";
+
+export let defaultLang = tempCodes.enus;
+
+export let superLangs = Object.values(tempCodes);
+
+export const getSuperLangs = () => superLangs;
+
+export const initLanguages = function (siteInfo) {
+  let configInvaild = false;
+  try {
+    let languages = siteInfo.sitePO.language.split(',');
+    langsMenu = langsMenu.filter(item => languages.includes(item.langCode));
+    if (!langsMenu.length) {
+      configInvaild = true;
+    }
+  } catch (e) {
+    configInvaild = true;
+  }
+  if (configInvaild) {
+    langsMenu = langsMenu.filter(item => item.langCode === defaultLang);
+  }
+  superLangs = langsMenu.map(item => item.langCode);
+  if (!superLangs.includes(defaultLang)) {
+    defaultLang = superLangs[0];
+  }
+  return superLangs;
+}
+
+export function formatLangCode(locale) {
+  if (!locale) {
+    return defaultLang;
+  }
+  if (superLangs.includes(locale)) {
+    return locale;
+  }
+  return tempCodes[locale.toLowerCase()] || defaultLang;
+}
+
+const cacheLangKey = window.cache_language_key || "cache_lang_config";
+
+export function getLangConfig() {
+  // 获取缓存中的lang数据
+  let config = null;
+  try {
+    config = JSON.parse(localStorage.getItem(cacheLangKey));
+  } catch (e) {
+    console.log(e);
+  }
+  if (config && config.version !== version) {
+    config = null;
+    localStorage.removeItem(cacheLangKey);
+  }
+  if (
+    config &&
+    config.locale &&
+    config.messages &&
+    config.messages[config.locale]
+  ) {
+    return config;
+  }
+  return {
+    locale: null,
+    version,
+    messages: Object.assign({}, (config && config.messages) || {}, {
+      "th-TH": enus,
+    }),
+  };
+}
+
+export function setLangConfig(configObj) {
+  localStorage.setItem(cacheLangKey, JSON.stringify(configObj));
+}
+
+
+export default {
+  tempCodes,
+  countryNameMap,
+  langsMenu,
+  version,
+  defaultLang,
+  superLangs,
+  formatLangCode,
+  getLangConfig,
+  setLangConfig
+}
--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,20 +1,53 @@
 import { createI18n } from 'vue-i18n'
-import enus from './langs/en-US/index.js'
-import zhtw from './langs/zh-TW/index.js'
+import { getLangConfig, defaultLang, setLangConfig, formatLangCode, version } from "./config";
 
+const config = getLangConfig();
+const loadedLanguages = Object.keys(config.messages);
+
+// const i18n = createI18n({
+//   legacy: false,
+//   locale: 'en',
+//   messages: {
+//     en: enus,
+//     zhtw: zhtw,
+//   }
+// })
 
 const i18n = createI18n({
   legacy: false,
-  locale: 'en',
-  messages: {
-    en: enus,
-    zhtw: zhtw,
-  }
+  locale: config.locale || defaultLang,
+  messages: config.messages,
 })
 
-function loadLanguageAsync(value) {
-  console.log('loadLanguageAsync',value);
-  return 'loadLanguageAsync'+value
+function loadLanguageAsync(locale, cacheLang = false) {
+  locale = formatLangCode(locale);
+  if (!cacheLang && config.locale) {
+    // 有设置过语言的不设置为默然语言
+    return;
+  }
+  if (i18n.locale === locale || loadedLanguages.includes(locale)) {
+    return Promise.resolve(setLocale({ locale, cacheLang }));
+  }
+  return import(`./langs/${locale}/index.js`)
+    .then((messages) => {
+      i18n.setLocaleMessage(locale, messages.default);
+      loadedLanguages.push(locale);
+      setLocale({ locale, messages, cacheLang });
+    })
+    .catch((e) => {
+      console.warn(`load ${locale} fail`, e);
+    });
+}
+
+function setLocale({ locale, messages, cacheLang }) {
+  i18n.locale = locale;
+  cacheLang && (config.locale = locale);
+  config.version = version;
+  upLangSetterHander(locale);
+  if (messages) {
+    config.messages[locale] = messages.default;
+  }
+  setLangConfig(config);
 }
 
 export { loadLanguageAsync }
