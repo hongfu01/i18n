8cde56262c5c13c2f8e7bef6322b082ca31dee2f
esan <apple@esanMac-mini.local>
2024-02-17 14:50:42 +0800

    export langsMenu

--- src/i18n/assets.js
+++ src/i18n/assets.js
@@ -1,4 +1,4 @@
-import enUs from '../assets/image/countrys/en-US.png';
+import enUs from '../assets/image/countrys/en-US';
 // import zhTw from '../assets/image/countrys/zh-TW.png';
 
 export default {
