158a0a9ea1a3dc4cf751be72e9a9a4000ec492e6
esan <apple@esanMac-mini.local>
2024-02-17 11:51:35 +0800

    index.js import 新增副檔名

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,5 +1,5 @@
 import { createI18n } from 'vue-i18n'
-import { getLangConfig, defaultLang, setLangConfig, formatLangCode, version } from "./config";
+import { getLangConfig, defaultLang, setLangConfig, formatLangCode, version } from "./config.js";
 
 const config = getLangConfig();
 const loadedLanguages = Object.keys(config.messages);
