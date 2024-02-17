d46fc073cd0498d007f9893eab1cdf9266ce8ec4
esan <apple@esanMac-mini.local>
2024-02-17 14:19:37 +0800

    import image

--- src/i18n/assets.js
+++ src/i18n/assets.js
@@ -1,7 +1,7 @@
 import enUs from '../assets/image/countrys/en-US.png';
-import zhTw from '../assets/image/countrys/zh-TW.png';
+// import zhTw from '../assets/image/countrys/zh-TW.png';
 
 export default {
     'en-US': enUs,
-    'zh-TW': zhTw,
+    // 'zh-TW': zhTw,
 }
