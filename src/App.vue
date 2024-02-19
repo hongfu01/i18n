464fbe9d278e4b30aa6ce0be25b6daf4ab63439c
esan <apple@esanMac-mini.local>
2024-02-19 14:57:23 +0800

    ad loadLanguageAsync

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
-import asserts from './i18n/assets.js';
-console.log('asserts',asserts);
 </script>
 
 <template>
